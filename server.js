bcb01a0a161a6a484b681848438fe1ec409bbe16
Ubuntu <ubuntu@ip-172-31-46-89.ec2.internal>
2025-06-15 09:23:39 +0000

    EC2 updates

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
+const path = require('path');
 
 connectDB();
 
 // INNIT MIDDLEWARE
-app.use(express.json({ extended: false }));
-
+//app.use(express.json({ extended: false }));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 // PRELIM ROUTE
-// app.get('/', (req, res) => res.send('Hello world'));
+//app.get('/', (req, res) => res.send('Hello world'));
 
 // DEFINE ROUTES
 app.use('/', require('./routes/deepheme'));
 app.use('/users/', require('./routes/users'));
 app.use('/auth/', require('./routes/auth'));
 
-const PORT = process.env.PORT || 5555;
-app.listen(PORT, () => console.log(`server started on ${ PORT }`));
+//app.get('/', (req, res) => res.send('Hello world'));
+
+// const PORT = process.env.PORT || 5555;
+const PORT = 5555;
+app.listen(PORT, '0.0.0.0', () => console.log(`server started on ${ PORT }`));
